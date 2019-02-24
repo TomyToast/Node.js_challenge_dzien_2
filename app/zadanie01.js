bc76e3eda11478e28f342f57aac2faa05b3b3c91
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-24 19:37:45 +0100

    finished task: zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,23 @@
 //Twój kod
+require('./data/zadanie01/input.json');
+
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+    if (err === null) {
+        let JSON_DATA = JSON.parse(data);
+        let sum = JSON_DATA.reduce( (total, current) => total + current, 0);
+        return (
+            // console.log(sum);
+            fs.writeFile('./data/zadanie01/sum.txt', `${sum}`, err => {
+                if (err === null) {
+                console.log('Ok, zapisałeś dane!', sum);
+                } else {
+                    console.log('Coś się zjebało!', err);
+                }
+            })
+        )
+    } else {
+        console.log(err);
+    }
+});
