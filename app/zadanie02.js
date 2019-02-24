1480435317f36916da4e271461d60285c366128c
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-24 19:46:53 +0100

    finished task zadanie02

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,20 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readdir('./data/zadanie02', (err, files) => {
+    if (err === null){
+        console.log('Lista plików: ');
+        files.forEach(file => {
+            fs.readFile(`./data/zadanie02/${file}`, 'utf8', (err, data) => {
+                if (err === null){
+                    console.log(data);
+                } else {
+                    console.log('Błąd! : ', err);
+                }
+            })
+            console.log(file);
+        });
+    } else {
+        console.log('Błąd podczas listowania katalogu!', err);
+    }
+})
